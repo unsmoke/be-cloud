555f9f02ba2f5259c2856cbdfccace33275e0885
Raihan Romzi <raihan.rakhman16@gmail.com>
2024-05-23 08:29:24 +0700

    remove host

--- src/main.mjs
+++ src/main.mjs
@@ -4,9 +4,9 @@ import { web } from "./app/web.mjs";
 import { logger } from "./app/logging.mjs";
 
 
-const PORT = process.env.PORT || 8000
-const HOST = process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0';
+const PORT = process.env.PORT || 8080
+// const HOST = process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0';
 
-web.listen(PORT, HOST, () => {
-    logger.info(`Server is up and listening on ${HOST}:${PORT}`);
+web.listen(PORT, /*HOST,*/ () => {
+    logger.info(`Server is up and listening on ${PORT}`);
 });
