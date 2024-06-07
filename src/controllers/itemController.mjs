958c0f8597ac7668ff5c321512907a80ab31dabc
nathanaelpganata <nathanaelpg8@gmail.com>
2024-06-08 00:32:01 +0700

    fix: item api

--- src/controllers/itemController.mjs
+++ src/controllers/itemController.mjs
@@ -2,9 +2,9 @@ import itemService from '../services/itemService.mjs'
 import { responseSuccess } from '../utils/responseAPI.mjs'
 import { success } from '../utils/messageSuccess.mjs'
 
-const getAllItem = async (req, res, next) => {
+const getAllItems = async (req, res, next) => {
     try {
-        const result = await itemService.getAllItem()
+        const result = await itemService.fetchAllItems()
         res.status(success.HTTP.CODE.OK).send(
             responseSuccess(success.HTTP.CODE.OK, success.HTTP.STATUS.OK, result)
         )
@@ -13,10 +13,10 @@ const getAllItem = async (req, res, next) => {
     }
 }
 
-const getItem = async (req, res, next) => {
+const getItemDetail = async (req, res, next) => {
     try {
         const itemId = req.params.item_id
-        const result = await itemService.getItemDetail(itemId)
+        const result = await itemService.fetchItemDetail(itemId)
         res.status(success.HTTP.CODE.OK).send(
             responseSuccess(success.HTTP.CODE.OK, success.HTTP.STATUS.OK, result)
         )
@@ -25,4 +25,39 @@ const getItem = async (req, res, next) => {
     }
 }
 
-export default { getItem, getAllItem }
+const createNewItem = async (req, res, next) => {
+  try {
+      const itemData = req.body;
+      const result = await itemService.createItem(itemData);
+      res.status(success.HTTP.CODE.CREATED).send(
+          responseSuccess(success.HTTP.CODE.CREATED, success.HTTP.STATUS.CREATED, result)
+      );
+  } catch (e) {
+      next(e);
+  }
+};
+
+const updateItem = async (req, res, next) => {
+  try {
+      const itemId = req.params.item_id;
+      const itemData = req.body;
+      const result = await itemService.modifyItem(itemId, itemData);
+      res.status(success.HTTP.CODE.OK).send(
+          responseSuccess(success.HTTP.CODE.OK, success.HTTP.STATUS.OK, result)
+      );
+  } catch (e) {
+      next(e);
+  }
+};
+
+const deleteItem = async (req, res, next) => {
+  try {
+      const itemId = req.params.item_id;
+      await itemService.removeItem(itemId);
+      res.status(success.HTTP.CODE.NO_CONTENT).send();
+  } catch (e) {
+      next(e);
+  }
+};
+
+export default { getAllItems, getItemDetail, createNewItem, updateItem, deleteItem };
