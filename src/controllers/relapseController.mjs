a688ff97c472a1b836716cfd3dc72ef01c082a06
Raihan Romzi <raihan.rakhman16@gmail.com>
2024-06-18 16:36:23 +0700

    fix bug

--- src/controllers/relapseController.mjs
+++ src/controllers/relapseController.mjs
@@ -1,14 +1,18 @@
-import relapseService from '../services/relapseService.mjs'
+import relapseService from "../services/relapseService.mjs";
+import { responseSuccess } from "../utils/responseAPI.mjs";
+import { success } from "../utils/messageSuccess.mjs";
 
 const postRelapse = async (req, res, next) => {
-    try {
-        const result = await relapseService.handleRelapse(req)
-        res.status(201).json(result)
-    } catch (error) {
-        next(error)
-    }
-}
+  try {
+    const result = await relapseService.handleRelapse(req);
+    res.status(success.HTTP.CODE.OK).send(
+      responseSuccess(success.HTTP.CODE.OK, success.HTTP.STATUS.OK, result)
+    );
+  } catch (error) {
+    next(error);
+  }
+};
 
 export default {
-    postRelapse,
-}
+  postRelapse
+};
